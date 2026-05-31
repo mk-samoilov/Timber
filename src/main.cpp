eea34b356e9822c099c57ab85ced5870e9bcff1c
mk-samoilov <me@mk-samoilov.ru>
2026-05-31 21:17:32 +0300

    main render logic & background sprite loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,38 @@
-int main() {
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+    VideoMode vm(1920, 1080);
+    RenderWindow window(vm, "Timber!", Style::Fullscreen);
+
+    Texture textureBackground;
+
+    textureBackground.loadFromFile("assets/graphics/background.png");
+
+    Sprite spriteBackgound;
+
+    spriteBackgound.setTexture(textureBackground);
+
+    spriteBackgound.setPosition(0, 0);
+
+    while (window.isOpen())
+    {
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
+
+
+
+
+        window.clear();
+
+        window.draw(spriteBackgound);
+
+        window.display();
+    }
+
     return 0;
 }
