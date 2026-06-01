e0fd5c3d4535cfc5a75d246097008d6afd68ea01
mk-samoilov <me@mk-samoilov.ru>
2026-06-01 10:37:34 +0300

    add statics tree & bee

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,31 @@ int main()
     VideoMode vm(1920, 1080);
     RenderWindow window(vm, "Timber!", Style::Fullscreen);
 
-    Texture textureBackground;
+    window.setFramerateLimit(120);
 
+    Texture textureBackground;
     textureBackground.loadFromFile("assets/graphics/background.png");
 
     Sprite spriteBackgound;
-
     spriteBackgound.setTexture(textureBackground);
-
     spriteBackgound.setPosition(0, 0);
+
+    Texture textureTree;
+    textureTree.loadFromFile("assets/graphics/tree.png");
+
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+
+    Texture textureBee;
+    textureBee.loadFromFile("assets/graphics/bee.png");
+
+    Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+
+    bool beeActive = false;
+    float beeSpeed = 0.0f;
 
     while (window.isOpen())
     {
@@ -30,6 +46,8 @@ int main()
         window.clear();
 
         window.draw(spriteBackgound);
+        window.draw(spriteTree);
+        window.draw(spriteBee);
 
         window.display();
     }
