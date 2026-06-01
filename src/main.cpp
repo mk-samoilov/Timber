e97c835a1accc274ebcca38fec6ae596a5f64e7c
mk-samoilov <me@mk-samoilov.ru>
2026-06-01 11:05:29 +0300

    add statics clouds

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,28 @@ int main()
     bool beeActive = false;
     float beeSpeed = 0.0f;
 
+    Texture textureCloud;
+    textureCloud.loadFromFile("assets/graphics/cloud.png");
+
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    Sprite spriteCloud3;
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
+
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
+    
     while (window.isOpen())
     {
         if (Keyboard::isKeyPressed(Keyboard::Escape))
@@ -46,7 +68,13 @@ int main()
         window.clear();
 
         window.draw(spriteBackgound);
+       
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+
         window.draw(spriteTree);
+        
         window.draw(spriteBee);
 
         window.display();
